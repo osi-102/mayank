7de18c063ba9b07447b79cfac96e932a12e676c3
Mayank Chauhan <2003mayankchauhan@gmail.com>
2024-07-18 15:59:45 +0530

    refactor: Update link paths in data.js
    This commit updates the link paths in the data.js file to remove the leading slash ("/") and add the corresponding route names. This ensures that the links in the navigation menu work correctly and navigate to the desired pages.

--- src/components/data.js
+++ src/components/data.js
@@ -1,10 +1,10 @@
 const data = [
-  { id: 1, title: "Home", link: "/" },
-  { id: 2, title: "About", link: "/about" },
-  { id: 3, title: "Technologies", link: "/technologies" },
-  { id: 4, title: "Experience", link: "/experience" },
-  { id: 5, title: "Projects", link: "/projects" },
-  { id: 6, title: "Contact", link: "/contact" },
+  { id: 1, title: "Home", link: "" },
+  { id: 2, title: "About", link: "about" },
+  { id: 3, title: "Technologies", link: "technologies" },
+  { id: 4, title: "Experience", link: "experience" },
+  { id: 5, title: "Projects", link: "projects" },
+  { id: 6, title: "Contact", link: "contact" },
 ];
 
 export default data;

1f58e3bcaf99864bc93cfdfa025bbf9eec6ad56c
Mayank Chauhan <2003mayankchauhan@gmail.com>
2024-10-05 15:06:17 +0530

    refactor: Update dependencies and smooth scrolling behavior
    - Update react-router-hash-link to version 2.4.3
    - Update package.json and package-lock.json
    - Add smooth scrolling behavior to index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,28 +6,28 @@ import  data  from "./data";
 // import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
 
-  const handleClick = (e, link) => {
-    e.preventDefault();
-    const targetElement = document.querySelector(link);
-    if (targetElement) {
-      const offset = -85;
-      const elementPosition = targetElement.getBoundingClientReact().top;
-      const offsetPosition = elementPosition + window.scrollY + offset;
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-    }
-    setIsOpen(false);
-  };
+//   const handleClick = (e, link) => {
+//     e.preventDefault();
+//     const targetElement = document.querySelector(link);
+//     if (targetElement) {
+//       const offset = -85;
+//       const elementPosition = targetElement.getBoundingClientReact().top;
+//       const offsetPosition = elementPosition + window.scrollY + offset;
+//       window.scrollTo({
+//         top: offsetPosition,
+//         behavior: "smooth",
+//       });
+//     }
+//     setIsOpen(false);
+//   };
 
-  return (
+  // return (
     // <nav className="border-b border-neutral-900 rounded-4xl pb-4 sticky top-5 z-50 mb-20 flex items-center justify-between shadow-black shadow-xl bg-neutral-800">
     //   {/* Desktop toggleMenu */}
     //   <div className="flex items-center justify-between w-full px-4 lg:px-8">
@@ -86,47 +86,52 @@ const Navbar = () => {
     //     )}
     //   </div>
     // </nav>
-    <nav className="border-b border-neutral-900 rounded-4xl pb-4 sticky top-5 z-50 mb-20 flex items-center justify-between shadow-black shadow-xl bg-neutral-800">
-      <div className="flex items-center justify-between w-full px-4 lg:px-8">
-        <div className="flex items-center">
-          <img className="w-16 rounded-4xl mt-4" src={logo} alt="logo" />
-        </div>
-        <div className="flex items-center lg:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div className="hidden lg:flex items-center gap-4 text-2xl">
+
+
+
+
+    
+    // <nav className="border-b border-neutral-900 rounded-4xl pb-4 sticky top-5 z-50 mb-20 flex items-center justify-between shadow-black shadow-xl bg-neutral-800">
+    //   <div className="flex items-center justify-between w-full px-4 lg:px-8">
+    //     <div className="flex items-center">
+    //       <img className="w-16 rounded-4xl mt-4" src={logo} alt="logo" />
+    //     </div>
+    //     <div className="flex items-center lg:hidden">
+    //       <button onClick={toggleMenu} className="text-white focus:outline-none">
+    //         {isOpen ? (
+    //           <svg
+    //             className="w-6 h-6"
+    //             fill="none"
+    //             stroke="currentColor"
+    //             viewBox="0 0 24 24"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //           >
+    //             <path
+    //               strokeLinecap="round"
+    //               strokeLinejoin="round"
+    //               strokeWidth={2}
+    //               d="M6 18L18 6M6 6l12 12"
+    //             />
+    //           </svg>
+    //         ) : (
+    //           <svg
+    //             className="w-6 h-6"
+    //             fill="none"
+    //             stroke="currentColor"
+    //             viewBox="0 0 24 24"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //           >
+    //             <path
+    //               strokeLinecap="round"
+    //               strokeLinejoin="round"
+    //               strokeWidth={2}
+    //               d="M4 6h16M4 12h16m-7 6h7"
+    //             />
+    //           </svg>
+    //         )}
+    //       </button>
+    //     </div>
+        {/* <div className="hidden lg:flex items-center gap-4 text-2xl">
           <ul className="flex flex-row m-6 mt-8 gap-4">
             {data.map((item, index) => (
               <li key={index} className="hover:text-cyan-500">
@@ -138,12 +143,12 @@ const Navbar = () => {
                 {/* <Link to={item.link} className="cursor-pointer">
                   {item.title}
                 </Link> */}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      {isOpen && (
+      //         </li>
+      //       ))}
+      //     </ul>
+      //   </div>
+      // </div> */}
+      {/* {isOpen && (
         <div className="lg:hidden flex flex-col items-center bg-neutral-800 w-full px-4">
           <ul className="flex flex-col gap-4 py-4 text-xl">
             {data.map((item) => (
@@ -159,9 +164,9 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-      )}
-    </nav>
-  );
+      )} */}
+//     </nav>
+//   );
 };
 
 export default Navbar;
